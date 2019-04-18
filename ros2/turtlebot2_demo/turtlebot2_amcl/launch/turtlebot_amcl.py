6b75af74467cb2ed30ea0b0713874344deac1bb6
ros2 <pantphotiou@gmail.com>
2019-04-18 20:19:57 +0300

    fixed amcl launch file

--- ros2/turtlebot2_demo/turtlebot2_amcl/launch/turtlebot_amcl.py
+++ ros2/turtlebot2_demo/turtlebot2_amcl/launch/turtlebot_amcl.py
@@ -17,7 +17,7 @@ import os
 import sys
 
 from ament_index_python.packages import get_package_share_directory
-from launch,legacy import LaunchDescriptor
+from launch.legacy import LaunchDescriptor
 from launch.legacy.exit_handler import restart_exit_handler
 from launch.legacy.launcher import DefaultLauncher
 from launch.legacy.output_handler import ConsoleOutput
