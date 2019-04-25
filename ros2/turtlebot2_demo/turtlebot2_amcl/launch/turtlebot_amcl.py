addac8609606def713dd77700f119375d5abb3ea
ros2 <pantphotiou@gmail.com>
2019-04-25 15:47:32 +0300

    corrected tf_tree

--- ros2/turtlebot2_demo/turtlebot2_amcl/launch/turtlebot_amcl.py
+++ ros2/turtlebot2_demo/turtlebot2_amcl/launch/turtlebot_amcl.py
@@ -57,23 +57,6 @@ def launch(launch_descriptor, argv):
     )
     package = 'tf2_ros'
     ld.add_process(
-        # The XYZ/Quat numbers for base_link -> camera_rgb_frame are taken from the
-        # turtlebot URDF in
-        # https://github.com/turtlebot/turtlebot/blob/931d045/turtlebot_description/urdf/sensors/astra.urdf.xacro
-        cmd=[
-            get_executable_path(
-                package_name=package, executable_name='static_transform_publisher'),
-            '-0.087', '-0.0125', '0.287',
-            '0', '0', '0', '1',
-            'base_link',
-            'camera_rgb_frame'
-        ],
-        name='static_tf_pub_base_rgb',
-        exit_handler=restart_exit_handler,
-    )
-    package = 'tf2_ros'
-    package = 'tf2_ros'
-    ld.add_process(
         # The XYZ/Quat numbers for base_link -> base_footprint
         cmd=[
             get_executable_path(
@@ -86,6 +69,23 @@ def launch(launch_descriptor, argv):
         name='static_tf_pub_base_link_footprint',
         exit_handler=restart_exit_handler,
     )
+    package = 'tf2_ros'
+    ld.add_process(
+        # The XYZ/Quat numbers for base_link -> camera_rgb_frame are taken from the
+        # turtlebot URDF in
+        # https://github.com/turtlebot/turtlebot/blob/931d045/turtlebot_description/urdf/sensors/astra.urdf.xacro
+        cmd=[
+            get_executable_path(
+                package_name=package, executable_name='static_transform_publisher'),
+            '-0.087', '-0.0125', '0.287',
+            '0', '0', '0', '1',
+            'base_footprint',
+            'camera_rgb_frame'
+        ],
+        name='static_tf_pub_base_rgb',
+        exit_handler=restart_exit_handler,
+    )
+
     package = 'tf2_ros'
     ld.add_process(
         # The XYZ/Quat numbers for camera_rgb_frame -> camera_depth_frame are taken from the
